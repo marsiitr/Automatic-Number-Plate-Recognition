ea731494bf1b15ea85f383656eee244cf9ca0e27
kush1920 <60808886+kush1920@users.noreply.github.com>
2020-07-28 19:41:33 +0530

    Added object detection

---
+++ src/object_detect.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+
+
+def get_output_layers(net):
+    
+    layer_names = net.getLayerNames()
+    
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+classes = None
+
+with open("yolov3.txt", 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+net = cv2.dnn.readNet("yolov3.weights" , "yolov3.cfg.txt")
+
+def extract_object(path=None , img=None) :
+    if img is None:
+        img = cv2.imread(path)
+    
+    objects=[]
+    
+    Width = img.shape[1]
+    Height = img.shape[0]
+    scale = 0.00392
+
+    blob = cv2.dnn.blobFromImage(img, scale, (416,416), (0,0,0), True, crop=False)
+    
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+    
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+    
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if class_id == 2 or class_id == 3 or class_id == 5:
+                if confidence > 0.5:
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
+    
+    
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+    
+    for n,i in enumerate(indices):
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        ob = img[round(y):round(y+h),round(x):round(x+w)]
+        objects.append([round(x),round(y),round(w),round(h)])
+        # cv2.imwrite("objects/"+str(n)+'_'+path , ob)
+        #draw_prediction(img, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+    return objects
+    # cv2.imshow("object_detection" , img)
+# extract_object("licensed_car155.jpg")
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
